7303e8de59ce66ce094f5dd40c12e5b06f604b92
sbspootis_windows <jonehawes@yahoo.ca>
2025-03-13 17:53:56 -0400

    Started font and text system. Updated both Linux and Windows buildsystems.

---
+++ src/t_font.cpp
@@ -0,0 +1,40 @@
+#include "t_font.hpp"
+
+
+
+nthp::text::Font::Font() {
+        memset(fontData, 0, nthp::text::fontByteLength);
+}
+
+
+
+int nthp::text::Font::importFont(const char* filename) {
+        std::fstream file;
+        file.open(filename, std::ios::in | std::ios::binary);
+
+        if(file.fail()) {
+                PRINT_DEBUG_ERROR("Unable to open font file [%s]; File inaccessible.\n", file);
+                return 1;
+        }
+
+        file.read((char*)fontData, nthp::text::fontByteLength);
+        file.close();
+
+        return 0;
+}
+
+
+int nthp::text::Font::exportFont(const char* filename) {
+        std::fstream file;
+        file.open(filename, std::ios::out | std::ios::binary);
+
+        if(file.fail()) {
+                PRINT_DEBUG_ERROR("Unable to open font file [%s]; Permission denied.\n");
+                return 1;
+        }
+
+        file.write((char*)&fontData, nthp::text::fontByteLength);
+        file.close();
+        
+        return 1;
+}
